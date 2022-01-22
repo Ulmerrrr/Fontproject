66acb2f2ab03baf2bb9ea5d6236fea3c41d93774
Ulmerrrr <1697807263@qq.com>
2022-01-22 11:06:21 +0800

    删除

--- hhh-music/src/store/mutation-types.js
+++
@@ -1,2 +0,0 @@
-// 这里可以定义一些常量，这些常量可以在mutations里面用作函数名。
-export const SET_PLAYLIST = 'SET_PLAYLIST'
--- hhh-music/src/store/state.js
+++
@@ -1,7 +0,0 @@
-// 这里定义一些状态
-// 定义了一个state对象，里面存了一些状态
-const state = {
-  fullScreen: false,
-  playlist: ['huang']
-}
-export default state
